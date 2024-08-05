dbe8304ee877f61c26f0b14318ed2d606cb8c954
BunyaweeS <156067258+BunyaweeS@users.noreply.github.com>
2024-08-05 10:02:08 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.css'
+export default function App(){
+  return (
+    <>
+    <h2>Developer Thai.com</h2>
+    <div className='title'>React & React Native</div>
+    </>
+  )
+  const divStyle={
+    
+    color:'red',backgroundColor:'powderblue',
+    fontSize:'larger',
+    padding:'3px',
+  }
+  return <div style={divStyle}>Hello React</div>
+}
