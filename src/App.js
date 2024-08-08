6e6e4eea2c488da6e5815bea8fd0611d686605bb
BunyaweeS <s6403051623127@email.kmutnb.ac.th>
2024-08-09 00:46:01 +0700

    Function Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './style.css'
 import Calendar from './class.componets'
+import { Header,Content,Footer } from './func-componets'
 
 export default function App(){
   return (
     <>
-  
+  <Header/>
+  <Content/>
+  <Footer/>
   <p><center><Calendar/></center></p>
   
   </>
