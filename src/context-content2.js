9df219b73ad58262ce588e0930315150705e226a
BunyaweeS <156067258+BunyaweeS@users.noreply.github.com>
2024-09-02 11:17:55 +0700

    context-content2

---
+++ src/context-content2.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { userContext } from "./Context"
+
+export default function Content2(){
+    let [user,setUser]= React.useState('')
+
+    const contentStyle={
+        backgroundColor:'#ddd',
+        textAlign:'center',
+        margin:10,
+        padding:10
+    }
+    const onClickSignin=(event)=>{
+        event.preventDefault()
+        setUser('Tom Jerry')
+    } 
+    return(
+        <div style={contentStyle}>
+            {
+                (user)
+                ?<span>Hello{[user,setUser]}</span>
+                :<span>Please<a href=""onClick={onClickSignin}>
+                    Signin</a></span>
+            }
+        </div>
+    )
+}
